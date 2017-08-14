312de803523377d6db9e0fdce497257504c3cae8
github username <github email address>
2017-08-14 16:52:45 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,21 @@
 // Code Invitation Component Here
+import React from 'react'
+
+export default class Invitation extends React.Component {
+
+
+  render(){
+    let anyChildren = React.Children.map(this.props.children, child =>
+      {
+        return child
+      })
+    return(
+      <div>
+        <h1>
+        You've been invited!
+        </h1>
+        {anyChildren}
+      </div>
+    )
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,21 @@
 // Code ThemedDecoration Component Here
+
+import React from 'react'
+
+export default class ThemedDecoration extends React.Component {
+
+
+  render(){
+    let anyChildren = React.Children.map(this.props.children, child =>
+      {
+        return React.cloneElement( child, {
+          className: this.props.theme
+        })
+      })
+    return(
+      <div>
+        {anyChildren}
+      </div>
+    )
+  }
+}
